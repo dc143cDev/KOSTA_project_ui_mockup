1e74bf86016be2155fe36f5024ead2e1c68cf16f
dc143cDev <devc143c@gmail.com>
2022-12-06 16:48:50 +0900

    git init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { AppBar, Tabs, Tab, Typography, Box } from '@mui/material';
+import { Container } from 'react-bootstrap';
+import TabTutorial from './components/mui_navbar/navbar';
+import Footer from './components/Layout/Footer';
 
-function App() {
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container fixed>
+        
+      </Container>
+      <Footer></Footer>
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/pages/funding.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Funding = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	}}
+	>
+	<h1>펀딩asdfasdf</h1>
+	</div>
+);
+};
+
+export default Funding;
+
